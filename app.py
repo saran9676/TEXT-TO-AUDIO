d1a550aa81863704d8a20fc8ba9abe41bd2d42f3
saran9676 <93899471+saran9676@users.noreply.github.com>
2023-07-12 12:25:05 +0530

    Updataing name

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from gtts import gTTS 
 from playsound import playsound
 audio="speech.mp3"
-text=''' Good morning sir,my name is sharan.I am from Dharmavaram.
+text=''' Good morning sir,my name is sharan teja reddy.I am from Dharmavaram.
 I have completed my btech in vardhaman engineering college,Inter in narayana college and schooling in narayana.
 coming to my family,My father is a government teacher,My mother is a house wife,2 sisters are working as a software engineers in banglore.
 My hobbies are playing cricket,badminton,watching Movies.
